8585cc92e3728d882b50ba8211e403819d780715
Filipp Gareev <f.gareev@agroassist.ru>
2025-03-16 22:22:21 +0300

    rm redundants

--- src/main.rs
+++ src/main.rs
@@ -21,59 +21,53 @@ fn get_fields_from_json() -> Result<String, io::Error> {
 }
 
 fn main() {
-    let fields_json = get_fields_from_json();
-    match fields_json {
-        Ok(s) => {
-            let fields: Vec<Field> = serde_json::from_str(&s).expect("не смог собрать поля");
+    let fields_json = get_fields_from_json().expect("Ошибка десериализации");
+    let fields: Vec<Field> = serde_json::from_str(&fields_json).expect("Не смог собрать поля");
 
-            let mut vector_of_placemarks = Vec::new();
-            for field in fields {
-                let kml_geometry = kml::types::Geometry::from(field.contour.clone());
-                let sf_name_tag = Element {
-                    name: String::from("sf_name"),
-                    content: Some(field.superfield_name.clone()),
-                    attrs: HashMap::new(),
-                    children: Vec::new(),
-                };
-                let f_name_tag = Element {
-                    name: String::from("f_name"),
-                    content: Some(field.name.clone()),
-                    attrs: HashMap::new(),
-                    children: Vec::new(),
-                };
-                let area_tag = Element {
-                    name: String::from("area"),
-                    content: Some(field.area_etalon_hectare.to_string().clone()),
-                    attrs: HashMap::new(),
-                    children: Vec::new(),
-                };
-                let children_vec = vec![sf_name_tag, f_name_tag, area_tag];
-                let kml_placemark = Kml::Placemark(kml::types::Placemark {
-                    name: Some(field.superfield_name.clone()),
-                    description: None,
-                    geometry: Some(kml_geometry),
-                    style_url: None,
-                    attrs: HashMap::new(),
-                    children: children_vec,
-                });
-                vector_of_placemarks.push(kml_placemark);
-            }
-
-            let document = Kml::KmlDocument(kml::types::KmlDocument {
-                version: kml::KmlVersion::V23,
-                attrs: HashMap::new(),
-                elements: vector_of_placemarks,
-            });
-
-            let mut buf = Vec::new();
-            kml::KmlWriter::from_writer(&mut buf)
-                .write(&document)
-                .expect("ошибка в записи файла");
-            fs::File::create("foot.kml")
-                .expect("не получилось создать файл")
-                .write_all(&buf)
-                .expect("не получилось записать файл");
-        }
-        Err(e) => panic!("лох, у тебя ошибка: {}", e),
+    let mut vector_of_placemarks = Vec::new();
+    for field in fields {
+        let kml_geometry = kml::types::Geometry::from(field.contour.clone());
+        let sf_name_tag = Element {
+            name: String::from("sf_name"),
+            content: Some(field.superfield_name.clone()),
+            attrs: HashMap::new(),
+            children: Vec::new(),
+        };
+        let f_name_tag = Element {
+            name: String::from("f_name"),
+            content: Some(field.name),
+            attrs: HashMap::new(),
+            children: Vec::new(),
+        };
+        let area_tag = Element {
+            name: String::from("area"),
+            content: Some(field.area_etalon_hectare.to_string()),
+            attrs: HashMap::new(),
+            children: Vec::new(),
+        };
+        let kml_placemark = Kml::Placemark(kml::types::Placemark {
+            name: Some(field.superfield_name),
+            description: None,
+            geometry: Some(kml_geometry),
+            style_url: None,
+            attrs: HashMap::new(),
+            children: vec![sf_name_tag, f_name_tag, area_tag],
+        });
+        vector_of_placemarks.push(kml_placemark);
     }
+
+    let document = Kml::KmlDocument(kml::types::KmlDocument {
+        version: kml::KmlVersion::V23,
+        attrs: HashMap::new(),
+        elements: vector_of_placemarks,
+    });
+
+    let mut buf = Vec::new();
+    kml::KmlWriter::from_writer(&mut buf)
+        .write(&document)
+        .expect("ошибка в записи файла");
+    fs::File::create("foot.kml")
+        .expect("не получилось создать файл")
+        .write_all(&buf)
+        .expect("не получилось записать файл");
 }
